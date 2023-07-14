dfd7163d202af4f45d7883712219157d2f064588
sezyxx <mrsaddam2806@gmail.com>
2023-07-14 15:19:17 +0700

    task day10Bs

--- Js/myProject.js
+++ Js/myProject.js
@@ -1,24 +1,24 @@
-const emptyProject = () => {
+function emptyFormAlert() {
   let projectName = document.getElementById("projectName").value;
   let startDate = document.getElementById("input-start-date").value;
   let endDate = document.getElementById("input-end-date").value;
   let description = document.getElementById("input-description").value;
-  let image = document.getElementById("input-image").value;
+  let image = document.getElementById("input-imgae").value;
 
-  if (projectName == "") {
-    return alert("Project Name cant empty");
-  } else if (startDate == "") {
-    return alert("When did you start this project");
-  } else if (endDate == "") {
-    return alert("Whent did you end this project");
-  } else if (description == "") {
+  if (projectName = " ") {
+    return alert("Please input your project name");
+  } else if (startDate = " ") {
+    return alert("When did you start this project ?");
+  } else if (endDate) {
+    return alert("When did you end this project ?");
+  } else if (description = " ") {
     return alert("Please describe this project");
-  } else if (image == "") {
+  } else if (image = " ") {
     return alert("Please input your image project");
   }
-};
+}
 
-const projectData = [];
+let projectData = [];
 
 function postProject(event) {
   event.preventDefault();
@@ -29,141 +29,100 @@ function postProject(event) {
   let description = document.getElementById("input-description").value;
   let image = document.getElementById("input-image").files;
 
-  const nodeIcon = ' <i class="fa-brands fa-node-js"></i>';
+  const nodeIcon = '<i class="fa-brands fa-node"></i>';
   const reactIcon = '<i class="fa-brands fa-react"></i>';
   const golangIcon = '<i class="fa-brands fa-golang"></i>';
   const jsIcon = '<i class="fa-brands fa-square-js"></i>';
 
-  let nodeChecked = document.getElementById("nodeCheck").checked
+  let nodeIconChecked = document.getElementById("nodeCheck").checked
     ? nodeIcon
     : "";
-  let reactChecked = document.getElementById("reactCheck").checked
+  let reactIconChecked = document.getElementById("reactCheck").checked
     ? reactIcon
     : "";
-  let golangChecked = document.getElementById("goCheck").checked
+  let golangIconChecked = document.getElementById("goCheck").checked
     ? golangIcon
     : "";
-  let jsChecked = document.getElementById("jsCheck").checked ? jsIcon : "";
+  let jsIconChecked = document.getElementById("jsCheck").checked ? jsIcon : "";
 
   image = URL.createObjectURL(image[0]);
+  console.log(image);
 
-  let timeDistance = getDistanceTime()
+  function distanceTime() {
+    let startDate = new Date(document.getElementById("input-start-date").value);
+    let endDate = new Date(document.getElementById("input-end-date").value);
+    let dist = new Date(endDate) - new Date(startDate);
+    let days = Math.floor(dist / (1000 * 3600 * 24));
+    let weeks = Math.floor(dist / (1000 * 3600 * 24 * 7));
+    let months = Math.floor(dist / (1000 * 3600 * 24 * 30));
+    let years = Math.floor(dist / (1000 * 3600 * 24 * 30 * 12));
+
+    if (years == 1) {
+      return `${years} year`;
+    } else if (years > 0) {
+      return `${years} years`;
+    } else if (months == 1) {
+      return `${months} month`;
+    } else if (months > 0) {
+      return `${months} months`;
+    } else if (weeks == 1) {
+      return `${weeks} week`;
+    } else if (weeks > 0) {
+      return `${weeks} weeks`;
+    } else if (days == 1) {
+      return `${days} day`;
+    } else if (days > 0) {
+      return `${days} days`;
+    }
+  }
+
+  let getDistanceTime = distanceTime();
 
   let projectPreviewCard = {
     projectName,
-    startDate,
-    endDate,
-    timeDistance,
+    getDistanceTime,
     description,
-    nodeChecked,
-    reactChecked,
-    golangChecked,
-    jsChecked,
+    nodeIconChecked,
+    reactIconChecked,
+    golangIconChecked,
+    jsIconChecked,
     image,
   };
 
   projectData.push(projectPreviewCard);
   console.log(projectData);
-  renderProject();
-};
 
-function renderProject() {
-  document.getElementById("cardProject").innerHTML = "";
+  pushProject();
+}
+
+function pushProject() {
+  document.getElementById("card-project").innerHTML = "";
 
   for (let i = 0; i < projectData.length; i++) {
-    let formattedStartDate = formatDate(projectData[i].startDate);
-    let formattedEndDate = formatDate(projectData[i].endDate);
-
-    document.getElementById("cardProject").innerHTML += `
-        <div class="cardProject" id="cardProject">
-                <div class="imgCard">
-                    <img src="${projectData[i].image}" />
-                </div>
-                <div class="title">
-                    <a href="detailProject.html">
-                        <h3>${projectData[i].projectName}</h3>
-                    </a>
-                </div>
-                <div class="duration">
-                    <p class="durationDate">Start - End : ${formattedStartDate} | ${formattedEndDate}</p>
-                    <p class="durationTime">Duration : ${projectData[i].timeDistance}</p>
-                </div>
-                <div class="content">
-                    <p>${projectData[i].description}</p>
-                </div>
-                <div class="tech">
-                     ${projectData[i].nodeChecked}
-                     ${projectData[i].reactChecked}
-                     ${projectData[i].golangChecked}
-                     ${projectData[i].jsChecked}
-                </div>
-                <div class="buttons">
-                    <button type="button">Edit</button>
-                    <button type="button">Delete</button>
-                </div>
-            </div>`;
+    document.getElementById("card-project").innerHTML += `
+    <div id="card-project" class="my-project" style="padding-top: 50px;">
+    <div class="project-flex">
+    <div class="project-flex-box">
+    <img src="${projectData[i].image}">
+    <a href="detailProject.html" target="_blank">
+    <h3 class="project-name">${projectData[i].projectName}</h3>
+    </a>
+    <h5 class="duration">Duration: ${projectData[i].getDistanceTime}</h5>
+    <p class="descriotion">${projectData[i].description}</p>
+    <p id="timeAgo" style="float: right; margin: 10px;"></p>
+    <p class="project-icon">
+            ${projectData[i].nodeIconChecked}
+            ${projectData[i].reactIconChecked}
+            ${projectData[i].golangIconChecked}
+            ${projectData[i].jsIconChecked}
+            </p>
+            <div class="project-button">
+            <button type="button">edit</button>
+            <button type="button">delete</button>
+            </div>
+            </div>
+            </div>
+            </div>
+            `;
   }
-};
-
-
- function getDistanceTime() {
-  const milliesecond = 1000
-  const second = 3600
-  const day = 24
-  const week = 7
-  const month = 30
-  const year = 12
-
-
-   let startDate = new Date(document.getElementById("input-start-date").value);
-   let endDate = new Date(document.getElementById("input-end-date").value);
-   let distance = new Date(endDate) - new Date(startDate);
-   let days = Math.floor(distance / (milliesecond * second * day));
-   let weeks = Math.floor(distance / (milliesecond * second * day * week));
-   let months = Math.floor(distance / (milliesecond * second * day * month));
-   let years = Math.floor(distance / (milliesecond * second * day * month * year));
-
-   if (years == 1) {
-     return `${years} year`;
-   } else if (years > 0) {
-     return `${years} years`;
-   } else if (months == 1) {
-     return `${months} month`;
-   } else if (months > 0) {
-     return `${months} months`;
-   } else if (weeks == 1) {
-     return `${weeks} week`;
-   } else if (weeks > 0) {
-     return `${weeks} weeks`;
-   } else if (days == 1) {
-     return `${days} day`;
-   } else if (days > 0) {
-     return `${days} days`
-   }
- }
-
- function formatDate(dateString) {
-   const monthsName = [
-     "January",
-     "February",
-     "March",
-     "April",
-     "May",
-     "June",
-     "July",
-     "August",
-     "September",
-     "October",
-     "November",
-     "December",
-   ];
-
-   const date = new Date(dateString);
-   const monthIndex = date.getMonth();
-   const monthName = monthsName[monthIndex];
-
-   const day = date.getDate();
-   const year = date.getFullYear();
-
-   return `${monthName} ${day}, ${year}`;
- }
+}
